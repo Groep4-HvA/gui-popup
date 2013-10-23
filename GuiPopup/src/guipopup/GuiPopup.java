ab9934e9816f9dcace8f525e578868a6d9efcdea
cigircii <fatih@cigirci.com>
2013-10-23 14:32:45 +0200

    addLuggage added

--- GuiPopup/src/guipopup/GuiPopup.java
+++ GuiPopup/src/guipopup/GuiPopup.java
@@ -1,19 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package guipopup;
 
-/**
- *
- * @author sean
- */
+
 public class GuiPopup {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        addLuggage gui = new addLuggage();
+        
+        gui.setVisible(true);
+        gui.setResizable(false); 
+   }
 }
