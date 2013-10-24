7efb9ddd0a25301dd1d73c527e8b17174f1eac77
Harry0088 <harrychinsee87@gmail.com>
2013-10-24 10:59:54 +0200

    Harry

--- GuiPopup/src/guipopup/GuiPopup.java
+++ GuiPopup/src/guipopup/GuiPopup.java
@@ -5,9 +5,11 @@ public class GuiPopup {
 
     public static void main(String[] args) {
         addLuggage gui = new addLuggage();
-         addMedewerker gui2 = new addMedewerker();
+        addMedewerker gui2 = new addMedewerker();
+        addPassenger gui3 = new addPassenger();
         
         gui.setVisible(true);
         gui2.setVisible(true); 
+        gui3.setVisible(true);
    }
 }
